3dc777a1ed3fc7d1d5065957ed5f80471aa762ae
SelivanovAN <superexpulsion@mail.ru>
2018-06-26 16:34:11 +0300

    сделал правки переменных в модуле УТИЛ и СЛАЙДЕР

--- js/create-wizards.js
+++ js/create-wizards.js
@@ -6,12 +6,12 @@
   var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 
-  window.wizards = []; // масиив для вставки магов
+  var wizards = []; // масиив для вставки магов
   var COUNT_WIZARDS = 4;
 
   var appendWizard = function () {
     for (var j = 0; j < COUNT_WIZARDS; j++) { // цикл для генерации магов и вставки в массив wizards
-      window.wizards.push({
+      wizards.push({
         name: WIZARD_NAMES[window.util.getRandomIndex(0, WIZARD_NAMES.length - 1)] + ' ' + WIZARD_SUR_NAME[window.util.getRandomIndex(0, WIZARD_SUR_NAME.length - 1)],
         coatColor: WIZARD_COAT_COLOR[window.util.getRandomIndex(0, WIZARD_COAT_COLOR.length - 1)],
         eyesColor: WIZARD_EYES_COLOR[window.util.getRandomIndex(0, WIZARD_EYES_COLOR.length - 1)]
@@ -20,4 +20,5 @@
   };
 
   appendWizard();
+  window.wizards = wizards;
 })();
--- js/util.js
+++ js/util.js
@@ -4,14 +4,6 @@ window.util = (function () {
   return {
     getRandomIndex: function (min, max) { // функция генерации случайных данных
       return Math.floor(Math.random() * (max - min)) + min;
-    },
-
-    // openPopup: function () {
-    // заготовка
-    // },
-
-    // closePopup: function () {
-    //  заготовка
-    // }
+    }
   };
 })();
