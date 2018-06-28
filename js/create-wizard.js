132f09545d3f98872f91d064f52e04210f7b2871
SelivanovAN <superexpulsion@mail.ru>
2018-06-28 16:36:27 +0300

    Сделал первоначальную загрузку магов - надо править код

--- js/create-wizard.js
+++ js/create-wizard.js
@@ -1,27 +1,27 @@
 'use strict';
 
 (function () {
-  var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-  var WIZARD_SUR_NAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-  var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-  var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
-
-  var wizards = []; // масиив для вставки магов
-  var COUNT_WIZARDS = 4;
-
-  var appendWizard = function () {
-    for (var j = 0; j < COUNT_WIZARDS; j++) { // цикл для генерации магов и вставки в массив wizards
-      wizards.push({
-        name: WIZARD_NAMES[window.util.getRandomIndex(0, WIZARD_NAMES.length - 1)] + ' ' + WIZARD_SUR_NAME[window.util.getRandomIndex(0, WIZARD_SUR_NAME.length - 1)],
-        coatColor: WIZARD_COAT_COLOR[window.util.getRandomIndex(0, WIZARD_COAT_COLOR.length - 1)],
-        eyesColor: WIZARD_EYES_COLOR[window.util.getRandomIndex(0, WIZARD_EYES_COLOR.length - 1)]
-      });
-    }
-  };
-
-  appendWizard();
-
-  window.createWizard = {
-    wizardsArr: wizards
-  };
+  // var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+  // var WIZARD_SUR_NAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+  // var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+  // var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+  //
+  // var wizards = []; // масиив для вставки магов
+  // var COUNT_WIZARDS = 4;
+  //
+  // var appendWizard = function () {
+  //   for (var j = 0; j < COUNT_WIZARDS; j++) { // цикл для генерации магов и вставки в массив wizards
+  //     wizards.push({
+  //       name: WIZARD_NAMES[window.util.getRandomIndex(0, WIZARD_NAMES.length - 1)] + ' ' + WIZARD_SUR_NAME[window.util.getRandomIndex(0, WIZARD_SUR_NAME.length - 1)],
+  //       coatColor: WIZARD_COAT_COLOR[window.util.getRandomIndex(0, WIZARD_COAT_COLOR.length - 1)],
+  //       eyesColor: WIZARD_EYES_COLOR[window.util.getRandomIndex(0, WIZARD_EYES_COLOR.length - 1)]
+  //     });
+  //   }
+  // };
+  //
+  // appendWizard();
+  //
+  // window.createWizard = {
+  //   wizardsArr: wizards
+  // };
 })();
--- js/render-wizards.js
+++ js/render-wizards.js
@@ -8,25 +8,67 @@
   var renderWizard = function (wizard) { // функция по созданию ДОМ мага (одного мага)
     var wizardElement = similarWizardTemplate.cloneNode(true);
     wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+    // wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+    // wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
     return wizardElement;
   };
 
-  var fragment = document.createDocumentFragment(); // создаем контейнер для хранения ДОМ мага
+  // var fragment = document.createDocumentFragment(); // создаем контейнер для хранения ДОМ мага
+  //
+  // var appendWizardsInFragment = function () { // функция для генерации 4-х ДОМ магов и вставки в контейнер
+  //   for (var i = 0; i < window.createWizard.wizardsArr.length; i++) {
+  //     fragment.appendChild(renderWizard(window.createWizard.wizardsArr[i]));
+  //   }
+  //   var similarListElement = setupDialog.querySelector('.setup-similar-list');
+  //
+  //   similarListElement.appendChild(fragment); // вставляем из контейнера в разметку 4-х ДОМ магов
+  // };
+  //
+  // appendWizardsInFragment(); // запуск фунции по герации 4-х ДОМ магов и вставки в контейнер
+  //
+  // setupDialog.querySelector('.setup-similar').classList.remove('hidden'); // Вставка ДОМ магов из контейнера в разметку
 
-  var appendWizardsInFragment = function () { // функция для генерации 4-х ДОМ магов и вставки в контейнер
-    for (var i = 0; i < window.createWizard.wizardsArr.length; i++) {
-      fragment.appendChild(renderWizard(window.createWizard.wizardsArr[i]));
+  var similarListElement = setupDialog.querySelector('.setup-similar-list');
+
+  var successHandler = function (wizards) {
+    var fragment = document.createDocumentFragment();
+    var NUMBERS_WIZARDS = 4;
+
+    for (var i = 0; i < NUMBERS_WIZARDS; i++) {
+      fragment.appendChild(renderWizard(wizards[i]));
     }
-    var similarListElement = setupDialog.querySelector('.setup-similar-list');
 
-    similarListElement.appendChild(fragment); // вставляем из контейнера в разметку 4-х ДОМ магов
+    similarListElement.appendChild(fragment);
+
+    document.querySelector('.setup-similar').classList.remove('hidden');
   };
 
-  appendWizardsInFragment(); // запуск фунции по герации 4-х ДОМ магов и вставки в контейнер
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
 
-  setupDialog.querySelector('.setup-similar').classList.remove('hidden'); // Вставка ДОМ магов из контейнера в разметку
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  window.backend.load(successHandler, errorHandler);
+
+  // send information
+
+  var form = document.querySelector('.setup-wizard-form');
+
+  form.addEventListener('submit', function (evt) {
+    window.backend.save(new FormData(form), function () {
+      window.util.closePopup();
+    });
+    evt.preventDefault();
+  });
 
   window.renderWizards = {
     setupDialogElement: setupDialog
