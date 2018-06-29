cc32e39e0231a6b8e5079f4f16e3a21f9fe27c9d
SelivanovAN <superexpulsion@mail.ru>
2018-06-29 18:06:48 +0300

    Удалил лишние комментарии

--- js/render-wizards.js
+++ js/render-wizards.js
@@ -2,7 +2,6 @@
 
 (function () {
   var setupDialog = document.querySelector('.setup');
-  // setupDialog.classList.remove('hidden');
   var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
   var renderWizard = function (wizard) { // функция по созданию ДОМ мага (одного мага)
@@ -12,21 +11,6 @@
     wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
     return wizardElement;
   };
-
-  // var fragment = document.createDocumentFragment(); // создаем контейнер для хранения ДОМ мага
-  //
-  // var appendWizardsInFragment = function () { // функция для генерации 4-х ДОМ магов и вставки в контейнер
-  //   for (var i = 0; i < window.createWizard.wizardsArr.length; i++) {
-  //     fragment.appendChild(renderWizard(window.createWizard.wizardsArr[i]));
-  //   }
-  //   var similarListElement = setupDialog.querySelector('.setup-similar-list');
-  //
-  //   similarListElement.appendChild(fragment); // вставляем из контейнера в разметку 4-х ДОМ магов
-  // };
-  //
-  // appendWizardsInFragment(); // запуск фунции по герации 4-х ДОМ магов и вставки в контейнер
-  //
-  // setupDialog.querySelector('.setup-similar').classList.remove('hidden'); // Вставка ДОМ магов из контейнера в разметку
 
   var similarListElement = setupDialog.querySelector('.setup-similar-list');
 
